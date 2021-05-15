d4c0366c3c58a3afcf71c4ce5f0cf98d522dfffb
Carlos <anomik@hotmail.com>
2021-05-14 19:11:47 -0500

    My First Error xD

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i=kja$w1d-ftju#5h__a^0q**@e)*z%8&l$4p@iepr9)x&28#*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com', '.amazonaws.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'pantsu.pythonanywhere.com', '.amazonaws.com']
 
 
 # Application definition
